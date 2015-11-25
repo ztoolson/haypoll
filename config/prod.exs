c68a4618e2a0960310b00386aa9b66dc51d443b6
Zach Toolson <zach.toolson@mx.com>
2015-11-24 22:01:25 -0700

    use production config from Heroku ENV variables

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,18 @@ use Mix.Config
 # which you typically run after static files are built.
 config :haypoll, Haypoll.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "haypoll.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database for Heroku
+config :haypoll, Haypoll.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  user: System.get_env("DATABASE_URL"),
+  poll_size: 20
 
 # ## SSL Support
 #
@@ -58,4 +65,5 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# WE DON"T NEED THIS LINE FOR HEROKU
+# import_config "prod.secret.exs"
